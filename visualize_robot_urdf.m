fa06966df6bae070bd6392730b58c256e34dfc8d
StravopodisNikos <stravnik.rob@gmail.com>
2020-09-17 14:42:12 +0300

    file to visualize robot urdf

---
+++ visualize_robot_urdf.m
@@ -0,0 +1,18 @@
+% loads stl file to robot
+% robot urdf is built from /structure_synthesis/S0110110.xacro
+function [] = visualize_robot_urdf(robotURDFfile)
+
+[robot] = importrobot(robotURDFfile);  
+robot.DataFormat = 'column';
+robot.Gravity = [0 0 -9.80665];
+robot_figure = figure;
+%% Show reference anatomy robot
+figure(robot_figure);
+config = [0 0]';
+close all;
+show(robot,config,'PreservePlot',false);
+hold on;
+axis auto;
+box on;
+
+end
