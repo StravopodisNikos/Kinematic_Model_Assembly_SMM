572c9293b69f208e5a9108b572bae8f992f3f6eb
StravopodisNikos <stravnik.rob@gmail.com>
2021-01-14 12:16:59 +0200

    trying to sync files between laptop-grafeio-pc

--- investigate_dynamic_kinematic_isotropy/config_space_investigation_DCI.m
+++ investigate_dynamic_kinematic_isotropy/config_space_investigation_DCI.m
@@ -14,8 +14,52 @@ root_path = root_path(1);
 matfiles_4_11_path_relative_to_matlab_ws = fullfile('matlab_ws','Kinematic_Model_Assembly_SMM','investigate_dynamic_kinematic_isotropy','optimized-structure-anatomies-matfiles','MBS_structures','4_11',filesep);
 matfiles_4_11_library_path = strcat(root_path,matfiles_4_11_path_relative_to_matlab_ws); addpath(matfiles_4_11_path_relative_to_matlab_ws);
 
-%% STRUCTURE 1
+screws_path_relative_to_matlab_ws = fullfile('matlab_ws','screw_kinematics_library','screws',filesep);
+screws_library_path = strcat(root_path,screws_path_relative_to_matlab_ws); addpath(screws_library_path);
+% addpath('/home/nikos/matlab_ws/screw_kinematics_library/screws')
+util_path_relative_to_matlab_ws = fullfile('matlab_ws','screw_kinematics_library','util',filesep);
+util_library_path = strcat(root_path,util_path_relative_to_matlab_ws); addpath(util_library_path);
+% addpath('/home/nikos/matlab_ws/screw_kinematics_library/util')
+screw_dynamics_path_relative_to_matlab_ws = fullfile('matlab_ws','screw_dynamics',filesep);
+screw_dynamics_library_path = strcat(root_path,screw_dynamics_path_relative_to_matlab_ws); addpath(screw_dynamics_library_path);
+% addpath('/home/nikos/matlab_ws/screw_dynamics')
+Kinematic_Model_Assembly_SMM_path_relative_to_matlab_ws = fullfile('matlab_ws','Kinematic_Model_Assembly_SMM',filesep);
+Kinematic_Model_Assembly_SMM_library_path = strcat(root_path,Kinematic_Model_Assembly_SMM_path_relative_to_matlab_ws); addpath(Kinematic_Model_Assembly_SMM_library_path);
+% addpath('/home/nikos/matlab_ws/Kinematic_Model_Assembly_SMM')
+building_functions_path_relative_to_matlab_ws = fullfile('matlab_ws','Kinematic_Model_Assembly_SMM','building_functions',filesep);
+building_functions_library_path = strcat(root_path,building_functions_path_relative_to_matlab_ws); addpath(building_functions_library_path);
+% addpath('/home/nikos/matlab_ws/Kinematic_Model_Assembly_SMM/building_functions')
+synthetic_joints_tfs_path_relative_to_matlab_ws = fullfile('matlab_ws','Kinematic_Model_Assembly_SMM','synthetic_joints_tfs',filesep);
+synthetic_joints_tfs_library_path = strcat(root_path,synthetic_joints_tfs_path_relative_to_matlab_ws); addpath(synthetic_joints_tfs_library_path);
+% addpath('/home/nikos/matlab_ws/Kinematic_Model_Assembly_SMM/synthetic_joints_tfs')
+calculateFunctions_path_relative_to_matlab_ws = fullfile('matlab_ws','Kinematic_Model_Assembly_SMM','calculateFunctions',filesep);
+calculateFunctions_library_path = strcat(root_path,calculateFunctions_path_relative_to_matlab_ws); addpath(calculateFunctions_library_path);
+% addpath('/home/nikos/matlab_ws/Kinematic_Model_Assembly_SMM/calculateFunctions')
+ 
+%% I. LOAD STRUCTURE-ANATOMY DATA
 load('s1_4_11_optimized_structure_anatomies.mat');
 
+%% II. VISUALIZE STRUCTURE-ANATOMY
+
+%% III. BUILD STRUCTURE-ANATOMY @ reference configution
+% III.1 BUILD STRUCTURE @ REFERENCE ANTOMY
+[xi_ai_struct_ref,xi_pj_ref,g_ai_ref,g_pj_ref,gst0,~,~,wrong_string_structure] = structure_assembly_3dof(ga_structure,ga_assembly_parameters);
+
+anatomy_index = 1; % change anatomy index!
+optimal_anatomy = s1_ga_test_mult_4_11_20_opt_anat(anatomy_index,:); % change s{i} index!
+
+optimal_anatomy_structure_dependent = calculate_transformed_anatomy_vector(ga_structure,'3dof',optimal_anatomy);
+[~,xi_ai_struct_anat,M_s_com_k_i_anat,g_s_com_k_i_anat] = calculateCoM_ki_s_structure_anatomy_no_graph(ga_structure,ga_assembly_parameters,optimal_anatomy_structure_dependent,xi_pj_ref);
+[g_s_link_as_anat,M_s_link_as_anat] = calculateCoMmetalinks(M_s_com_k_i_anat,g_s_com_k_i_anat);
+[M_b_link_as_anat] = calculateMetalinkInertiaMatrixBody(g_s_link_as_anat,M_s_link_as_anat);
+
+%% IV. RUN C-SPACE INVESTIGATION
+step_angle = [0 0.1 0.1];
+theta_lim  = [pi (4*pi)/6 pi];
+PM_string = 'dci-all';
+[PM_Cspace,Th2,Th3] = configuration_space_theta23loops(step_angle,theta_lim,PM_string,xi_ai_struct_anat,g_s_link_as_anat,M_b_link_as_anat);
+
+%% v. PLOTS
+figure; surf(Th2,Th3,PM_Cspace')
 
 
--- investigate_dynamic_kinematic_isotropy/configuration_space_theta23loops.m
+++ investigate_dynamic_kinematic_isotropy/configuration_space_theta23loops.m
@@ -1,5 +1,46 @@
-function [PM_Cspace] = configuration_space_theta23loops(step_angle,theta_lim,PM_string)
-% Perforamnce measures available: 1. 'dci'
-%
+function [PM_Cspace,Th2,Th3] = configuration_space_theta23loops(step_angle,theta_lim,PM_string,xi_ai_anat,g_s_link_as_anat,M_b_link_as_anat)
+% Input: 1,2. 3x1 vectors of step/limit angle of active joints
+%          3. string that indicates the performance measure to calculate, yet available: 1. 'dci-off' 2. 'dci-diag' 3. 'dci-all' 
+%          4. active joint twists for optimized structure+anatomy!
+
+% Pre-allocate Memory
+theta2_vec = linspace(-theta_lim(2),theta_lim(2),((2*theta_lim(2))/step_angle(2))+1);
+theta3_vec = linspace(-theta_lim(3),theta_lim(3),((2*theta_lim(3))/step_angle(3))+1);
+array_size2 = size( theta2_vec,2 );
+array_size3 = size( theta3_vec,2 );
+
+PM_Cspace = zeros(array_size2,array_size3);
+[Th2,Th3] = meshgrid(theta2_vec,theta3_vec);
+
+% Loops
+ta2_cnt = 0;
+for ta2=-theta_lim(2):step_angle(2):theta_lim(2)
+    ta2_cnt = ta2_cnt +1;
+        ta3_cnt = 0;
+        for ta3=-theta_lim(3):step_angle(3):theta_lim(3)
+            ta3_cnt = ta3_cnt + 1;
+            
+            qa = [0.1 ta2 ta3];
+            
+            [J_b_sli] = calculateCoM_BodyJacobians_for_anat(xi_ai_anat, qa, g_s_link_as_anat );
+             
+            [M_b] = calculateGIM(J_b_sli,M_b_link_as_anat);
+            
+            %%  I.c Compute DCI value
+            switch PM_string
+                case 'dci-off'
+                    wx = [0 0 0 1 1 1]; % for off diagonal elements
+                    [PM_Cspace(ta2_cnt,ta3_cnt)] = calculateDynamicConditioningIndexDimLess_3DoF(M_b,wx);
+                case 'dci-diag'
+                    wx = [1 1 1 0 0 0]; % for diagonal elements
+                    [PM_Cspace(ta2_cnt,ta3_cnt)] = calculateDynamicConditioningIndexDimLess_3DoF(M_b,wx);
+               case 'dci-all'
+                    wx = [1 1 1 1 1 1]; % for diagonal elements
+                    [PM_Cspace(ta2_cnt,ta3_cnt)] = calculateDynamicConditioningIndexDimLess_3DoF(M_b,wx);
+                case 'DME'
+                    
+            end
+        end
+end
 
 end
